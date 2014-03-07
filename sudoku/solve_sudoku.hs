4919aa8f2df0524af39a50b964c19bed74ac638d
Vlad <sukhmel@users.noreply.github.com>
2014-03-07 16:20:33 +0300

    Update solve_sudoku.hs
    better parser for tasks
    Each significant line of task should contain at least one number (one '0' is fine). All non-digits are ignored.

--- sudoku/solve_sudoku.hs
+++ sudoku/solve_sudoku.hs
@@ -3,16 +3,39 @@ module Main (
 ) where
 
 import Sudoku
+import Data.Char (isDigit,isSpace)
 import System.IO (readFile)
 import Control.Exception (catch)
+import Control.Applicative((<$>),(<*>))
 import System.Environment (getArgs)
 
-parseTask name = fmap (read :: [Char] -> [[Int]]) $ readFile name
+parseTask :: FilePath -> IO [[Int]]
+parseTask name = fmap 
+                      ( map 
+                            ( map read 
+                                . filter (
+                                          (&&) 
+                                          <$> all isDigit
+                                          <*> not . null
+                                          )
+                            )
+                       . map words
+                       . filter (not . null) 
+                       . lines
+                       . filter (
+                                 (||) 
+                                 <$> isDigit
+                                 <*> isSpace
+                                )
+                       )
+                      $ readFile name
 
 main = do args <- getArgs
           case args of
                [name] -> do catch (do task <- parseTask name
-                                      printSudoku $ getSolution $ getTask task
+                                      printSudoku 
+                                                  $ getSolution
+                                                  $ getTask task
                                       return () )
                                   ((\_ -> putStrLn "could not read file")
                                       :: IOError -> IO ())
