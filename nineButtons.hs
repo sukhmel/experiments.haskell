b3d1be315b6f9c19e41bd4a8aab29e92c91e63e4
sukhmel <sukhmel.v@gmail.com>
2014-05-21 20:28:51 +0400

    Rewritten main window of `nineButtons`, to become Sudoku main frame. Written translation from panel coordinates to cartesian coordinates, used by Sudoku solver module.

--- nineButtons.hs
+++ nineButtons.hs
@@ -20,35 +20,47 @@ main = start buttonPad
 
 buttonPad ::  IO ()
 buttonPad = do
-       pad    <- frame [ text := "[1..9]" ]
+       pad    <- frame [ text := "Sudoku frame" ]
        noText <- staticText pad [text := ""]
-       btns   <- mapM (\n -> button pad
+       btns   <- replicateM 9
+               $ mapM (\n -> button pad
                              [ size := sz 40 40
                              , text := [n]])
                  ['1'..'9']
        set pad [ layout := column 5
-                    [ grid 0 0
-                    . map (map widget)
-                    . splitInGroupsOf 3
-                    $ btns
-                    , widget noText ]]
+                           [( grid 15 15
+                            . map ( map ( grid 0 0
+                                        . map ( map widget )
+                                        . splitInGroupsOf 3))
+                            . splitInGroupsOf 3) btns
+                           , widget noText]
+                    ]
        let networkDescription :: forall t. Frameworks t => Moment t ()
            networkDescription = do
                -- convert WxHaskell events to FRP events
                let eventify         :: (Commanding w) => [w] -> Moment t [Event t ()]
                    eventify widgets = forM widgets $ \x -> event0 x command
 
-               events <- eventify btns
+               events <- eventify . concat $ btns
 
-               let chosen :: Event t Int
+               let chosen :: Event t (Int, Int)
                    chosen = foldl1 union
-                          . zipWith (<$) [1..]
+                          . zipWith (<$) [(x,y) | x <- [1..9]
+                                                , y <- [1..9]]
                           $ events
 
-                   bSet   = stepper 0 chosen
+                   bSet   = stepper (-1,-1) chosen
                eSet   <- changes bSet
-               reactimate' $ fmap print
+               reactimate' $ fmap (print . positionToCoordinates)
                           <$> eSet
 
        network <- compile networkDescription
        actuate network
+
+positionToCoordinates :: (Int, Int) -> (Int, Int)
+positionToCoordinates
+          (glob, loc) = (x, y)
+        where x       = 3*x1 + x0
+              y       = 3*y1 + y0
+              (y1,x1) = (glob - 1) `divMod` 3
+              (y0,x0) = (loc  - 1) `divMod` 3
