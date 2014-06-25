3178d077fa91b683befcad350177fce6bff806c4
Vlad <sukhmel.v@gmail.com>
2014-06-26 03:23:58 +0400

    Working bounce from static objects. Although very complex method is used, real cases should be easier.

--- lets-program/3.hs
+++ lets-program/3.hs
@@ -2,21 +2,30 @@ import Graphics.Gloss.Interface.Pure.Game
 
 data Object = Moving { position :: Point
                      , velocity :: Point
-                     , sprite   :: Picture }
+                     , sprite   :: Picture
+                     }
             | Static { position :: Point
                      , inside   :: Point -> Bool
-                     , sprite   :: Picture }
+                     , sprite   :: Picture
+                     , bounce   :: Float
+                     , normal   :: Point
+                     }
 
 data World = World { objects      :: [Object]
-                   , acceleration :: Point }
+                   , acceleration :: Point 
+                   }
 
 draw :: World -> Picture
 draw w = pictures
-       . ((color red . line) [(0, 0), acceleration w] :)
-       . map (\ (Object (x,y) (vx, vy) sprite)
-             ->  translate x y (scale 0.5 0.5 sprite) )
+       . ((color red . polygon) [a, b, c, a] :)
+       . map (\ obj
+             ->  uncurry translate (position obj) (scale 0.5 0.5 $ sprite obj) )
        . objects
        $ w
+    where a@(ax, ay) = acceleration w
+          b@(bx, by) = (ay/m, -ax/m)
+          c          = (-bx, -by)
+          m          = sqrt (ax**2 + ay**2) / 5
 
 handleKey :: Event -> World -> World
 handleKey (EventKey c Down _ _) w = w'
@@ -32,23 +41,55 @@ handleKey _ w = w
 
 -- bounce off walls, reducing speed to emulate dissipation
 advanceStep :: Float -> World -> World
-advanceStep t w = w { objects = obstacles ++ map process population }
-        where (ax, ay) = acceleration w
-              process ( Moving ( x,  y) 
-                               (vx, vy)
-                                sprite)
-                       = let vx' = if x < -400 || x > 400 then -vx*0.7 else vx
-                             vy' = if y < -300 || y > 300 then -vy*0.7 else vy
-                         in  Object ( x + vx'*t,  y + vy'*t)
-                                    (vx' + ax*t, vy' + ay*t) sprite
+advanceStep t w = w { objects = obstacles ++ map process movables }
+        where (ax, ay)  = acceleration w
+              process m = case filter (flip inside $ position m) obstacles
+                               of []    -> move m
+                                  (s:_) -> calculate m s
+              calculate m s = let b        = bounce s
+                                  f        = inside s
+                                  k        = 2 * (nx*vx + ny*vy)
+                                  (nx, ny) = normal s
+                                  (vx, vy) = velocity m
+                                  pos'     = revert m f 1
+                                  vel'     = (b*(vx - k*nx), b*(vy - k*ny))
+                              in  move $ m { position = pos'
+                                           , velocity = vel'
+                                           }
+              move m@(Moving{ position = (x,y), velocity = (vx, vy)})
+                            = m{ position = ( x + vx*t,  y + vy*t)
+                               , velocity = (vx + ax*t, vy + ay*t)
+                               }
               population = objects w
               obstacles  = [ obj | obj@(Static {}) <- population]
               movables   = [ obj | obj@(Moving {}) <- population]
 
+revert m f k = if f pos'
+                  then pos
+                  else revert m f (k/2)
+    where pos'    = (px - k * vx / 2, py - k * vy / 2)
+          pos     = (px - k * vx, py - k * vy)
+          (vx,vy) = velocity m
+          (px,py) = position m 
 
-revert w f = 
-
-
+bigBlueLine = (color blue $ polygon [ (-3000,   0)
+                                    , ( 3000,   0)
+                                    , ( 3000, -10)
+                                    , (-3000, -10)
+                                    , (-3000,   0)
+                                    ]
+              )
+wallH y f = Static (0, y) 
+                   (f y . snd) 
+                   bigBlueLine
+                   0.9
+                   (0, 1)
+wallV x f = Static (x, 0) 
+                   (f x . fst) 
+                   (rotate 90 bigBlueLine)
+                   0.9
+                   (1, 0)
+          
 main :: IO ()
 main = do ghost <- loadBMP "ghost.bmp"
           play ( InWindow "Boo!"
@@ -56,7 +97,16 @@ main = do ghost <- loadBMP "ghost.bmp"
                           (100, 100) )
                ( greyN 0.5 )
                30
-               ( World [Moving (0, 0) (0, 0) ghost] (0, 0) )
+               ( World [ Moving (0, 0)
+                                (0, 0)
+                                ghost
+                       , wallH (-200) (>)
+                       , wallH ( 200) (<)
+                       , wallV (-200) (>)
+                       , wallV ( 200) (<)
+                       ]
+                       (0, 0)
+               )
                draw
                handleKey
                advanceStep
