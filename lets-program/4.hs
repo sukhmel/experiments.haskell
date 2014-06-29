780dab3394fae0b8f046cc58e4ffd831070f2d54
Vlad <sukhmel.v@gmail.com>
2014-06-29 19:10:43 +0400

    Used lens (only a small part, need more experiments with these). Added box-polygon constructor.

--- lets-program/4.hs
+++ lets-program/4.hs
@@ -1,83 +1,89 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 import Data.List
+import Control.Lens
 import Graphics.Gloss.Interface.Pure.Game
 
-data Object = Moving { position :: Point
-                     , velocity :: Point
-                     , sprite   :: Picture
+data Object = Moving { _position :: Point
+                     , _velocity :: Point
+                     , _sprite   :: Picture
                      }
-            | Static { position :: Point
-                     , inside   :: Point -> Bool
-                     , sprite   :: Picture
-                     , bounce   :: Float
-                     , normal   :: Point
+            | Static { _position :: Point
+                     , _inside   :: Point -> Bool
+                     , _sprite   :: Picture
+                     , _bounce   :: Float
+                     , _normal   :: Point
                      }
 
+makeLenses ''Object
+                     
 instance Show Object where
-    show m@(Moving{}) = 'M' : '@' : show (position m)
-    show s@(Static{}) = 'S' : '@' : show (position s) ++ "_|_" ++ show (normal s) ++ '&' : show (sprite s)
+    show m@(Moving{}) = 'M' : '@' : show (_position m)
+    show s@(Static{}) = 'S' : '@' : show (_position s) ++ "_|_" ++ show (_normal s) ++ '&' : show (_sprite s)
     
-data World = World { objects      :: [Object]
-                   , acceleration :: Point 
-                   , viscosity    :: Point
-                   , viscosityPow :: Point
+data World = World { _objects      :: [Object]
+                   , _acceleration :: Point 
+                   , _viscosity    :: Point
+                   , _viscosityPow :: Point
                    }
+
+makeLenses ''World
 
 draw :: World -> Picture
 draw w = pictures
-       . ((color red . polygon) [a, b, c, a] :)
-       . map (\ obj
-             ->  pictures . map (uncurry translate $ position obj)
-               $ [ sprite obj
-                 ])
-       . objects
-       $ w
-    where a@(ax, ay) = acceleration w
+       . (++ [(color red . polygon) [a, b, c, a]])
+       $ zipWith (\ pos spr -> pictures
+                             . map (uncurry translate pos)
+                             $ [spr])
+                 ( w ^.. objects . traversed . position)
+                 ( w ^.. objects . traversed . sprite)
+    where a@(ax, ay) = view acceleration w
           b@(bx, by) = (ay/m, -ax/m)
           c          = (-bx, -by)
           m          = sqrt (ax**2 + ay**2) / 5
 
 handleKey :: Event -> World -> World
 handleKey (EventKey c Down _ _) w = w'
-    where (x,y) = acceleration w
+    where (x,y) = view acceleration w
           a'    = case c of
                     (Char 'a') -> (x - 2, y)
                     (Char 'd') -> (x + 2, y)
                     (Char 'w') -> (x, y + 2)
                     (Char 's') -> (x, y - 2)
                     _          -> (x, y)
-          w'    = w { acceleration = a' }
+          w'    = acceleration .~ a' $ w
 handleKey _ w = w
 
 -- bounce off walls, reducing speed to emulate dissipation
 advanceStep :: Float -> World -> World
-advanceStep t w = w { objects = obstacles ++ map process movables }
-        where (ax, ay)  = acceleration w
-              process m = case filter (flip inside $ position m) obstacles
+advanceStep t w = w { _objects = obstacles ++ map process movables }
+        where (ax, ay)  = view acceleration w
+              process m = case filter (flip _inside $ _position m) obstacles
                                of []    -> move m
                                   (s:_) -> calculate m s
-              calculate m s = let b        = bounce s
-                                  f        = inside s
+              calculate m s = let b        = _bounce s
+                                  f        = _inside s
                                   k        = 2 * (nx*vx + ny*vy)
-                                  (nx, ny) = normal s
-                                  (vx, vy) = velocity m
+                                  (nx, ny) = _normal s
+                                  (vx, vy) = _velocity m
                                   pos'     = revert m f 1
                                   vel'     = (b*(vx - k*nx), b*(vy - k*ny))
-                              in  move $ m { position = pos'
-                                           , velocity = vel'
+                              in  move $ m { _position = pos'
+                                           , _velocity = vel'
                                            }
-              move m@(Moving{ position = (x,y), velocity = (vx, vy)})
+              move m@(Moving{ _position = (x,y), _velocity = (vx, vy)})
                             = let (rx, ry) = resistance m
-                              in  m{ position = ( x + t* vx,        y + t* vy      )
-                                   , velocity = (vx + t*(ax - rx), vy + t*(ay - ry))
+                              in  m{ _position = ( x + t* vx,        y + t* vy      )
+                                   , _velocity = (vx + t*(ax - rx), vy + t*(ay - ry))
                                    }
-              resistance m = let (vx, vy) = velocity m
+              resistance m = let (vx, vy) = _velocity m
                                  norm     = sqrt $ vx**2 + vy**2
-                                 (cx, cy) = viscosity w
-                                 (px, py) = viscosityPow w
+                                 (cx, cy) = _viscosity w
+                                 (px, py) = _viscosityPow w
                                  resist   = ( cx * vx * norm**(px - 1)
                                             , cy * vy * norm**(py - 1) )
                              in  resist
-              population = objects w
+              population = view objects w
               obstacles  = [ obj | obj@(Static {}) <- population]
               movables   = [ obj | obj@(Moving {}) <- population]
 
@@ -86,26 +92,8 @@ revert m f k = if f pos'
                   else revert m f (k/2)
     where pos'    = (px - k * vx / 2, py - k * vy / 2)
           pos     = (px - k * vx, py - k * vy)
-          (vx,vy) = velocity m
-          (px,py) = position m 
-
-bigBlueLine = (color blue $ polygon [ (-3000,   0)
-                                    , ( 3000,   0)
-                                    , ( 3000, -10)
-                                    , (-3000, -10)
-                                    , (-3000,   0)
-                                    ]
-              )
-wallH y f = Static (0, y) 
-                   (f y . snd) 
-                   bigBlueLine
-                   1.1
-                   (0, 1)
-wallV x f = Static (x, 0) 
-                   (f x . fst) 
-                   (rotate 90 bigBlueLine)
-                   1.1
-                   (1, 0)
+          (vx,vy) = _velocity m
+          (px,py) = _position m 
 
 triang [a, b, c] = let (ax, ay) = a
                        (bx, by) = b
@@ -141,10 +129,17 @@ triang [a, b, c] = let (ax, ay) = a
 repulsePoly as = map (triang . (++ [centre]) . take 2)
                . filter (not . null . drop 1)
                . tails
-               $ as 
+               $ as
     where centre = average as (0,0) 0
           average ((x,y):rest) (xs, ys) n = average rest (xs + x, ys + y) (n + 1)
           average [] (x, y) n = (x/n, y/n)
+
+emptyBox (x0, y0)
+         (x1, y1)
+         ( w,  h) = repulsePoly [(x0, y0), (x1, y0), (x1, y0 + h), (x0, y0 + h), (x0, y0)]
+                 ++ repulsePoly [(x0, y0), (x0, y1), (x0 + w, y1), (x0 + w, y0), (x0, y0)]
+                 ++ repulsePoly [(x1, y1), (x0, y1), (x0, y1 - h), (x1, y1 - h), (x1, y1)]
+                 ++ repulsePoly [(x1, y1), (x1, y0), (x1 - w, y0), (x1 - w, y1), (x1, y1)]
 
 main :: IO ()
 main = do ghost <- loadBMP "ghost.bmp"
@@ -156,16 +151,13 @@ main = do ghost <- loadBMP "ghost.bmp"
                ( World ( [ Moving (20, 20)
                                   (-10, -20)
                                   (scale 0.5 0.5 ghost)
-                         , triang [ (-201, -200), ( 201, -200), (   0, -250) ]
-                         , triang [ ( 200, -201), ( 200,  201), ( 250,    0) ]
-                         , triang [ ( 200,  200), (-201,  200), (   0,  250) ]
-                         , triang [ (-200,  201), (-200, -201), (-250,    0) ]
                          ]
                       ++ repulsePoly [ (  0,  50), ( 10,  10)
                                      , ( 50,   0), ( 10, -10)
                                      , (  0, -50), (-10, -10)
                                      , (-50,   0), (-10,  10)
-                                     , (  0,  50)] )
+                                     , (  0,  50)]
+                      ++ emptyBox (-200, -200) ( 200, 200) ( 10, 10) )
                        (0, 0)
                        (0.0, 0.0)
                        (1, 1) -- Neutonian environment: viscosity force depends
